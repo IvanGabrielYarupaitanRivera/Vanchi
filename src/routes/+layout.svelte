a8f572d2976407b790d068151e704652145299ca
Ivan Gabriel Yarupaitan Rivera <ivangyr321@gmail.com>
2025-11-30 02:35:42 -0500

    feat: estructurar el layout con Header y Footer en +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
-	
+	import Header from '$lib/components/Header.svelte';
+	import Footer from '$lib/components/Footer.svelte';
+
 	let { children } = $props();
 </script>
 
@@ -9,4 +11,12 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children()}
+<div class="flex min-h-screen flex-col">
+	<Header />
+
+	<main class="mx-auto w-full max-w-400 flex-1 px-4 lg:px-8">
+		{@render children()}
+	</main>
+
+	<Footer />
+</div>
