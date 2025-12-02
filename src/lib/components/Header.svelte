03dd30ff0b37864c80d4302cca71f67620182337
Ivan Gabriel Yarupaitan Rivera <ivangyr321@gmail.com>
2025-12-02 18:16:14 -0500

    feat: ajustar estilos y tamaños en el componente Header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -29,17 +29,17 @@
 	let open = $state(false);
 </script>
 
-<header class="sticky top-6 z-50 px-4">
+<header class="sticky top-2 z-50 lg:top-4">
 	<nav
-		class="navbar mx-auto max-w-6xl rounded-full border border-white/10 bg-base-300/60 p-4 backdrop-blur-lg transition-all duration-300"
+		class="navbar mx-auto max-w-6xl rounded-full border border-white/10 bg-base-300/60 px-4 backdrop-blur-lg transition-all duration-300"
 	>
 		<!-- Left: Brand / Logo -->
 		<div class="navbar-start">
 			<a href="/" class="group flex cursor-pointer items-center gap-2" draggable="false">
 				<!-- Logo Icon  -->
-				<img src={icon} alt="Vanchi Logo" class="h-12" draggable="false" />
+				<img src={icon} alt="Vanchi Logo" class="h-10" draggable="false" />
 				<!-- Logo Text -->
-				<img src={vanchi} alt="Vanchi Logo Text" class="hidden h-6 lg:block" draggable="false" />
+				<img src={vanchi} alt="Vanchi Logo Text" class="hidden h-4 lg:block" draggable="false" />
 			</a>
 		</div>
 
@@ -48,11 +48,11 @@
 			<ul class="menu menu-horizontal gap-2">
 				{#each navItems as item}
 					<li class="transition-transform duration-300 hover:-translate-y-0.5">
-						<a href={item.href} class="btn" style="--size: 3rem; --btn-p: 0.5rem;">
-							<div class="rounded-full bg-accent p-2">
-								<item.icon size={16} class="text-accent-content" />
+						<a href={item.href} class="btn" style="--size: 2.3rem; --btn-p: 0.2rem;">
+							<div class="rounded-full bg-accent p-1.5">
+								<item.icon size={14} class="text-accent-content" />
 							</div>
-							<span class="pr-1">{item.label}</span>
+							<span class="pr-1 text-xs">{item.label}</span>
 						</a>
 					</li>
 				{/each}
@@ -65,25 +65,25 @@
 			<a
 				href="/contacto"
 				class="btn hidden transition-transform duration-300 btn-primary hover:-translate-y-0.5 lg:flex"
-				style="--size: 3rem; --btn-p: 0.5rem;"
+				style="--size: 2.3rem; --btn-p: 0.2rem;"
 			>
-				<div class="rounded-full bg-base-100 p-2">
-					<ArrowUpRight size={16} class="text-base-content" />
+				<div class="rounded-full bg-base-100 p-1.5">
+					<ArrowUpRight size={14} class="text-base-content" />
 				</div>
-				<span class="pr-1">Contacto</span>
+				<span class="pr-1 text-xs">Contacto</span>
 			</a>
 
 			<!-- Mobile Dropdown (Visible only on small screens) -->
 			<button
 				class="btn btn-primary lg:hidden"
-				style="--size: 3rem; --btn-p: 0.5rem;"
+				style="--size: 2.3rem; --btn-p: 0.2rem;"
 				onclick={() => (open = !open)}
 				aria-label="Abrir menú de navegación móvil"
 			>
-				<div class="rounded-full bg-base-100 p-2">
-					<Menu size={16} class="text-base-content" />
+				<div class="rounded-full bg-base-100 p-1.5">
+					<Menu size={14} class="text-base-content" />
 				</div>
-				<span class="pr-1"> Menú </span>
+				<span class="pr-1 text-xs"> Menú </span>
 			</button>
 		</div>
 	</nav>
@@ -105,38 +105,36 @@
 		class="fixed top-0 right-0 z-70 h-full w-full max-w-2/3 rounded-l-2xl bg-base-100 lg:hidden"
 		transition:fly={{ x: 300 }}
 	>
-		<div class="flex h-full flex-col">
-			<header
-				class="flex items-center justify-between border-b border-secondary/50 px-4 py-4 shadow-sm"
-			>
-				<h2 class="font-semibold">Menú</h2>
+		<div class="flex h-full flex-col px-4 py-4">
+			<header class="flex items-center justify-between border-b border-secondary/50 pb-4 shadow-sm">
+				<h2 class="text-sm font-semibold">Menú</h2>
 				<button
 					type="button"
 					class="btn btn-primary"
-					style="--size: 2rem; --btn-p: 0.5rem;"
+					style="--size: 1.5rem; --btn-p: 0.2rem;"
 					onclick={() => (open = false)}
 					aria-label="Cerrar menú de navegación"
 				>
-					<X size={16} />
+					<X size={14} />
 				</button>
 			</header>
 
 			<nav class="my-4 flex-1 overflow-auto">
-				<ul class="flex flex-col gap-3">
+				<ul class="flex flex-col gap-1">
 					{#each navItems as item}
 						<li class="transition-transform duration-300 hover:-translate-y-0.5">
-							<a href={item.href} class="btn" style="--size: 3rem; --btn-p: 0.5rem;">
-								<div class="rounded-full bg-accent p-2">
-									<item.icon size={16} class="text-accent-content" />
+							<a href={item.href} class="btn" style="--size: 2.3rem; --btn-p: 0.2rem;">
+								<div class="rounded-full bg-accent p-1.5">
+									<item.icon size={14} class="text-accent-content" />
 								</div>
-								<span class="pr-1 text-sm">{item.label}</span>
+								<span class="pr-1 text-xs">{item.label}</span>
 							</a>
 						</li>
 					{/each}
 				</ul>
 			</nav>
 
-			<footer class="px-4 py-4 text-xs text-gray-500">
+			<footer class="text-xs text-gray-500">
 				<img src={vanchi} class="h-6" alt="Vanchi Logo Text" />
 			</footer>
 		</div>
