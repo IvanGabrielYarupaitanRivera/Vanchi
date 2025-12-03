a75d6638887edf4fdc46c46a95404f00aaae3b61
Ivan Gabriel Yarupaitan Rivera <ivangyr321@gmail.com>
2025-12-03 10:23:43 -0500

    feat: agregar el componente SocialProof en la página principal

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import Hero from '$lib/components/home/Hero.svelte';
+	import SocialProof from '$lib/components/home/SocialProof.svelte';
 </script>
 
 <Hero />
+<SocialProof />
