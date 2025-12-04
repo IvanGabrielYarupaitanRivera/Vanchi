c4132361b07b5c6bdb575d9b3b3edebb7ceb42c7
Ivan Gabriel Yarupaitan Rivera <ivangyr321@gmail.com>
2025-12-04 06:58:58 -0500

    feat: agregar componente FeaturedProjects en la página principal

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+	import FeaturedProjects from '$lib/components/home/FeaturedProjects.svelte';
 	import Hero from '$lib/components/home/Hero.svelte';
 	import SocialProof from '$lib/components/home/SocialProof.svelte';
 </script>
 
 <Hero />
 <SocialProof />
+<FeaturedProjects />
