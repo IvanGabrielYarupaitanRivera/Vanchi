fe513099004329c75d6ba620809f1e8640761b68
Ivan Gabriel Yarupaitan Rivera <ivangyr321@gmail.com>
2025-12-09 12:21:39 -0500

    feat: agregar el componente Services en la página principal

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import FeaturedProjects from '$lib/components/home/FeaturedProjects.svelte';
 	import Hero from '$lib/components/home/Hero.svelte';
+	import Services from '$lib/components/home/Services.svelte';
 	import SocialProof from '$lib/components/home/SocialProof.svelte';
 </script>
 
 <Hero />
 <SocialProof />
 <FeaturedProjects />
+<Services />
