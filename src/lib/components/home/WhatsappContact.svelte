6327d346fe323de651e948d6e59cd878d3defde6
Ivan Gabriel Yarupaitan Rivera <ivangyr321@gmail.com>
2025-12-11 18:58:36 -0500

    fix: eliminar el fondo borroso en la sección de contacto por WhatsApp

--- src/lib/components/home/WhatsappContact.svelte
+++ src/lib/components/home/WhatsappContact.svelte
@@ -30,10 +30,6 @@
 </script>
 
 <section id="contacto" class="relative py-16 lg:py-36" aria-label="Contacto por WhatsApp">
-	<div
-		class="pointer-events-none absolute top-1/2 left-1/2 h-52 w-52 -translate-x-1/2 -translate-y-1/2 rounded-full bg-primary/20 blur-3xl lg:h-96 lg:w-96"
-	></div>
-
 	<div class="relative container mx-auto px-4">
 		<div class="mx-auto max-w-3xl">
 			<article
