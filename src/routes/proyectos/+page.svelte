3d18557233b708ff7bc8594ae5b0f358d4781708
Ivan Gabriel Yarupaitan Rivera <ivangyr321@gmail.com>
2025-12-16 14:51:12 -0500

    feat: agregar imagen de proyectos en la meta descripción del SEO

--- src/routes/proyectos/+page.svelte
+++ src/routes/proyectos/+page.svelte
@@ -9,6 +9,7 @@
 	description="Explora los proyectos realizados por Vanchi, donde la innovación y la calidad se unen para crear soluciones digitales excepcionales."
 	keywords="Proyectos Vanchi, Portafolio de Proyectos, Desarrollo Web, Diseño de Software, Soluciones Digitales, Casos de Éxito, Innovación Tecnológica, Servicios de Desarrollo, Proyectos Destacados, Vanchi Proyectos"
 	url="https://vanchi.pro/proyectos"
+	image="https://vanchi.pro/images/proyectos.webp"
 />
 
 <Hero />
