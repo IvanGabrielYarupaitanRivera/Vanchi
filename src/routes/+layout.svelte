160af2414eb3d7a9a5a45a3d03beb0262c1fda74
Ivan Gabriel Yarupaitan Rivera <ivangyr321@gmail.com>
2025-12-16 17:05:15 -0500

    feat: eliminar componente SEO del layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,9 @@
 	import './layout.css';
 	import Header from '$lib/components/Header.svelte';
 	import Footer from '$lib/components/Footer.svelte';
-	import SEO from '$lib/components/SEO.svelte';
 
 	let { children } = $props();
 </script>
-
-<SEO />
 
 <div class="flex min-h-screen flex-col">
 	<Header />
