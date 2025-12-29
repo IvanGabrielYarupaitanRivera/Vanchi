d54c37ff09923dca58a4cfa9316072fb43f16a33
Ivan Gabriel Yarupaitan Rivera <ivangyr321@gmail.com>
2025-12-29 03:20:08 -0500

    feat: actualizar ruta en el sitemap para incluir junin360

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -4,7 +4,7 @@ export async function GET() {
 	const website = 'https://www.vanchi.pro/';
 
 	// Lista de tus rutas estáticas
-	const pages = ['', 'proyectos', 'ivan-yarupaitan-rivera'];
+	const pages = ['', 'proyectos', 'proyectos/junin360'];
 
 	const sitemap = `<?xml version="1.0" encoding="UTF-8" ?>
 <urlset
