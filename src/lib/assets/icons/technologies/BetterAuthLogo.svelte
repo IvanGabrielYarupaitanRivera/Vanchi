410d40683d835c116d2acd968d98fd2d3633241c
Ivan Gabriel Yarupaitan Rivera <ivangyr321@gmail.com>
2025-12-31 01:19:45 -0500

    feat: agregar nuevos íconos BetterAuth y GeminiIA en formato Svelte

---
+++ src/lib/assets/icons/technologies/BetterAuthLogo.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	let { size = 24 } = $props();
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	width={size}
+	height={size}
+	viewBox="0 0 100 100"
+	fill="none"
+>
+	<path d="M24.0964 14H0V85.7452H24.0964V14Z" fill="white" />
+	<path d="M99.9999 14H74.3975V85.7452H99.9999V14Z" fill="white" />
+	<path d="M99.2471 37.1179V14L50.9036 14V37.1179H99.2471Z" fill="white" />
+	<path d="M100 85.7448V62.627H50.9036V85.7448H100Z" fill="white" />
+	<path d="M50.9036 62.6266V37.1172H24.0963V62.6266H50.9036Z" fill="white" />
+</svg>
