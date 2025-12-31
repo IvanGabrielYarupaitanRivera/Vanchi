ec45aa5deab2633d7b75a3a6fc2604f8822c8f7c
Ivan Gabriel Yarupaitan Rivera <ivangyr321@gmail.com>
2025-12-31 17:39:37 -0500

    feat: agregar obstetraconecta a la lista de proyectos

--- src/lib/data/projects/index.ts
+++ src/lib/data/projects/index.ts
@@ -2,9 +2,11 @@ import type { CaseStudy } from '$lib/types/project';
 import { junin360 } from './junin360';
 import { mediroosevelt } from './mediroosevelt';
 import { farmape } from './farmape';
+import { obstetraconecta } from './obstetraconecta';
 
 export const projectsData: Record<string, CaseStudy> = {
 	junin360,
 	mediroosevelt,
-	farmape
+	farmape,
+	obstetraconecta
 };
