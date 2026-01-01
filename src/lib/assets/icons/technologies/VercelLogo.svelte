a13d97683929b41deb5a5c0e3625c5c838648235
Ivan Gabriel Yarupaitan Rivera <ivangyr321@gmail.com>
2025-12-31 23:17:27 -0500

    feat: agregar ícono de Vercel en formato Svelte

---
+++ src/lib/assets/icons/technologies/VercelLogo.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	let { size = 24 } = $props();
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24"
+	><path fill="#fff" d="M23 21.648H1L12 2.352z" /></svg
+>
