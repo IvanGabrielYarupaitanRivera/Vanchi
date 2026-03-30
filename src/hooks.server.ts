65934ab62cbb04297e698fb452efe99d18d25c3f
Ivan Gabriel Yarupaitan Rivera <ivangyr321@gmail.com>
2026-03-30 17:43:25 -0500

    refactor: eliminar redirección de WWW a CANONICAL_HOST en el manejador de eventos

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,16 +1,5 @@
 import type { Handle } from '@sveltejs/kit';
 
-const CANONICAL_HOST = 'vanchi.pro';
-const WWW_HOST = 'www.vanchi.pro';
-
 export const handle: Handle = async ({ event, resolve }) => {
-	const { url } = event;
-
-	if (url.hostname === WWW_HOST) {
-		const redirectUrl = new URL(url);
-		redirectUrl.hostname = CANONICAL_HOST;
-		return Response.redirect(redirectUrl, 301);
-	}
-
 	return resolve(event);
 };
